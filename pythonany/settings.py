6e7205f3e818e3039a5cb19ef3fbba4e1a4cc0bc
24neurons <momo123vmf@gmail.com>
2022-10-21 21:57:14 +0700

    Fix invidual apps' static resources

--- pythonany/settings.py
+++ pythonany/settings.py
@@ -124,8 +124,8 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 import os
-STATIC_URL = '/staticfiles/'
+STATIC_URL = 'staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles/')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR , 'media/')
