5f6789e5bfe70dc1144503925932b4fc9699aca3
24neurons <momo123vmf@gmail.com>
2022-10-21 09:59:12 +0700

    Change background config

--- Homepage/views.py
+++ Homepage/views.py
@@ -15,7 +15,7 @@ def index(request):
 		cacloai = SanPham.objects.filter(Loai__Dong=dong)
 		context['data'][dong.Ten_Tieng_Viet] = cacloai
 
-	return render(request, "Homepage/index.html" , context)
+	return render(request, "index.html" , context)
 
 def ProductLine(request , productLine):
 
@@ -25,14 +25,14 @@ def ProductLine(request , productLine):
     for loai in CacLoaiSanPham:
         cacsanpham = SanPham.objects.filter(Loai=loai)
         context['data'][loai.Ten_Tieng_Viet] = cacsanpham
-    return render(request , "Homepage/DongSanPham.html" , context)
+    return render(request , "DongSanPham.html" , context)
 
 def ProductType(request , productLine , productType):
 
     CacSanPham = SanPham.objects.filter(Loai__Ten=productType)
     context = {}
-    return render(request , "Homepage/LoaiSanPham.html" , context)
+    return render(request , "LoaiSanPham.html" , context)
 def OneProduct(request , productLine , productType , productCode):
     product = SanPham.objects.filter(id=productCode)
     context = {'product' : product}
-    return render(request , "Homepage/SanPham.html" , context)
+    return render(request , "SanPham.html" , context)
